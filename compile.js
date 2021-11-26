6c84d3f53a02400dd3ebdc5ef6577c6e5bf086d5
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-26 14:52:54 -0500

    created basic compile script

--- compile.js
+++ compile.js
@@ -1,1 +1,9 @@
 // compile code will go here
+const path = require('path');
+const fs = require('fs');
+const { SSL_OP_CIPHER_SERVER_PREFERENCE } = require('constants');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
