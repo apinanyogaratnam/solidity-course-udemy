c8b2426758e86665c59b6821fc5afd24d49f15f1
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-26 14:54:30 -0500

    added solc import

--- compile.js
+++ compile.js
@@ -1,7 +1,7 @@
 // compile code will go here
 const path = require('path');
 const fs = require('fs');
-const { SSL_OP_CIPHER_SERVER_PREFERENCE } = require('constants');
+const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
