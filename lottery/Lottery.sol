66ad39be4eab693e2f56f630e8975a0569d89374
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-28 14:28:22 -0500

    created enter player function

--- lottery/Lottery.sol
+++ lottery/Lottery.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] players;
 
     function Lottery() public {
+        // msg.sender is the address of the person
+        // who deploys the contract
         manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        // msg.sender is the player's address
+        // since they are involking the function
+        players.push(msg.sender);
     }
 }
