b583f3f9492276396f45ff20d73c40dd609a6487
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-27 22:28:14 -0500

    created deploy script to deploy on rinkeby testnet

--- deploy.js
+++ deploy.js
@@ -1,1 +1,25 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    process.env.SEED_PHRASE,
+    process.env.INFURA_URL
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({data: bytecode, arguments: ['Hi there!']})
+      .send({from: accounts[0], gas: '1000000'});
+    
+      console.log('Contract deployed to', result.options.address);
+      // prevent a hanging deployment
+      provider.engine.stop();
+};
+deploy();
